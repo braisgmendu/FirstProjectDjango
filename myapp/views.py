82c171f7713b41304dfc74749d1d19dec3fa8435
Brais GM <braisgmendu@gmail.com>
2024-08-27 12:38:19 +0200

    Cambios en formularios y actualizacion de estilo

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CommentForm
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def comment_view(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = CommentForm()
+    
+    return render(request, 'comment.html', {'form': form})
--- myproject/settings.py
+++ myproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['myapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [ "myapp/static",]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
